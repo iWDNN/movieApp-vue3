39f5e22f9094ad474a28db5131318fe71c59cccf
iWDNN <cksgh2064@daum.net>
2021-10-11 12:27:23 +0900

    netlify env

--- functions/movie.js
+++ functions/movie.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
+const { OMDB_API_KEY } = process.env //dotenv-webpack
 // import,export는 언제까지 브라우저에서 동작하기 위해 사용, nodeJS에서 끼리는 exports와 require로 주고 받음
 
 exports.handler = async function (event) {
   console.log(event)
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
   const url = id
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
